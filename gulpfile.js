0651b4aa09ec8b41c71ee7dc34403c17a63ab7d9
Raphael Araujo <raphaelwilliam.araujo@gmail.com>
2022-03-31 12:43:49 -0300

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+// add installed packages
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
+// compiles SASS and adds prefixes
 function compileSass() {
   return gulp
   .src('css/scss/*.scss')
@@ -13,12 +16,32 @@ function compileSass() {
     cascade: false
   }))
   .pipe(gulp.dest('css/'))
+  .pipe(browserSync.stream());
 }
 
+// gulp task for SASS function
 gulp.task('sass', compileSass);
 
-function watch() {
-  gulp.watch('css/scss/*.scss', compileSass);
+// function to start the browser server
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 }
 
-gulp.task('default', watch);
+// task to start the browser-sync
+gulp.task('browser-sync', browser);
+
+// gulp watch function
+function watch() {
+  gulp.watch('css/scss/*.scss', compileSass);
+  gulp.watch(['*html', '*.php']).on('change', browserSync.reload);
+}
+
+// starts watch task
+gulp.task('watch', watch);
+
+// default gulp task that starts watch and browser-sync
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
